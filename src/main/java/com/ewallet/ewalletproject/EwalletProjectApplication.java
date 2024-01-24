139272403dfc005c5491dabf39d948620c10bfe2
saurabh24gope <104162109+saurabh24gope@users.noreply.github.com>
2024-01-24 07:10:05 +0530

    project-added

---
+++ src/main/java/com/ewallet/ewalletproject/EwalletProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ewallet.ewalletproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwalletProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwalletProjectApplication.class, args);
+	}
+
+}
